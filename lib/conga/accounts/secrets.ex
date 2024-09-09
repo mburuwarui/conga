76c68e4a152357ae32f510aa492d6de2783be261
mburuwarui <mburuwarui@proton.me>
2024-09-09 21:06:38 +0300

    fix_secrets

--- lib/conga/accounts/secrets.ex
+++ lib/conga/accounts/secrets.ex
@@ -2,7 +2,7 @@ defmodule Conga.Accounts.Secrets do
   use AshAuthentication.Secret
 
   def secret_for([:authentication, :tokens, :signing_secret], Conga.Accounts.User, _) do
-    case Application.fetch_env(:example, CongaWeb.Endpoint) do
+    case Application.fetch_env(:conga, CongaWeb.Endpoint) do
       {:ok, endpoint_config} ->
         Keyword.fetch(endpoint_config, :secret_key_base)
 
